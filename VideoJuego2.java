dc6e93e616e8aa3c28fa43c15faceb6b79d67a55
leonhatches <jhatches@unsa.edu.pe>
2024-10-18 18:21:45 -0500

    Tercer commit con mostrar y mas metodos.

--- VideoJuego2.java
+++ VideoJuego2.java
@@ -4,8 +4,15 @@
         public static void main (String [] args)
         {
             Soldado [][] tablero = new Soldado [10][10];
-            crear           (tablero);
-            mostrarTabla    (tablero);
+            crear               (tablero);
+            mostrarTabla        (tablero);
+            mostrarMayorVida    (tablero);
+            double vida = mostrarPromedioVida (tablero);
+            mostrar(tablero);
+
+            // MOSTRAR VIDA DE TODOS LOS SOLDADOS
+            System.out.println ("\n\t| VIDA TOTAL DEL EJERCITO |\n");
+            System.out.println ("La vida total es: " + vida );
         }
 
         public static void crear (Soldado [][] tablero)
@@ -51,7 +58,7 @@
             
             for (int i = 0 ; i < tablero.length ; i++)
             {
-                 System.out.print (i+1);
+                System.out.print (i+1);
                 for (int j = 0 ; j < tablero[i].length ; j++)
                 {
                     if (tablero[i][j] == null)
@@ -72,5 +79,84 @@
         {
             return (int)(Math.random() * 5 + 1);
         }
+        
+        public static void mostrarMayorVida (Soldado[][]tablero)
+        {
+            int fila = 0, columna = 0;
+            
+            // LOS MAYORES VALORES INICIALES
+            for (int i = 0 ; i < tablero.length ; i++) {
+                for (int j = 0 ; j < tablero[i].length ; j++)
+                {
+                        if ( !(tablero[i][j] == null) )
+                        { fila = i; columna = j; break; }
+                }
+            }
+           
+           // BUSQUEDA DEL SOLDADO CON MAYOR VIDA 
+           for (int i = 0 ; i < tablero.length ; i++)
+           {
+                for (int j = 0 ; j < tablero[i].length ; j++){
+                    if ( !(tablero[i][j] == null) ) {
+                        if ( tablero[fila][columna].getVida() < tablero[i][j].getVida() )
+                        {
+                            fila = i;
+                            columna = j;
+                        }
+                    }        
+                }
+            }
+            
+            // MOSTRAR
+            System.out.println ("\n\t| SOLDADO CON MAYOR VIDA |\n");
+            System.out.println (tablero[fila][columna]+"\n");
+        }
+        
+        public static double mostrarPromedioVida (Soldado[][] tablero)
+        {
+           double suma = 0, cont = 0;
+           
+           // PROMEDIO DE VIDA
+           for (int i = 0 ; i < tablero.length ; i++)
+           {
+                for (int j = 0 ; j < tablero[i].length ; j++){
+                    if ( !(tablero[i][j] == null) )
+                    {
+                         suma += tablero[i][j].getVida();
+                         cont++;
+                    }       
+                }
+           }
+           
+            // MOSTRAR
+            System.out.println ("\n\t| PROMEDIO DE VIDA |\n");
+            System.out.println ("El promedio de vida es: " + (suma/cont) );
+            
+            return suma;
+        }
+        
+        public static void mostrarPorCreacion (Soldado[][]tablero)
+        {
+            
+        }
+        
+        public static void mostrar (Soldado [][] tablero)
+        {
+           System.out.println("\n\t| SOLDADOS - EJERCITO |");
+           for (int i = 0 ; i < tablero.length ; i++)
+           {
+                for (int j = 0 ; j < tablero[i].length ; j++){
+                    if ( !(tablero[i][j] == null) )
+                    {
+                         System.out.println("\n"+tablero[i][j]);
+                    }       
+                }
+           }
+        }
+        
+        
+        
+        
+        
     }
 
