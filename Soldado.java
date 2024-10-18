8807889893113cd3b6dc1b91c066df0a93d21378
leonhatches <jhatches@unsa.edu.pe>
2024-10-18 17:37:28 -0500

    Segundo commit, hasta mostrar tablero con datos ya inicializados.

--- Soldado.java
+++ Soldado.java
@@ -1,47 +1,41 @@
 
     public class Soldado
     {
-            private String  nombre, columna;
-            private int     vida, fila;
-            
+        private String  nombre, columna;
+        private int     vida, fila;
 
-            public void setNombre (String n) {
-                    nombre = n;
-            }
-            public void setVida (int v) {
-                    vida = v;
-            }
-            public void setFila (int f) {
-                    fila = f;
-            }
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            public void setColumna (String c) {
-                    columna = c;
-            }
 
-            public String getNombre ()
-            {	return nombre;	}
+        public void setNombre (String n) {
+                nombre = n;
+        }
+        public void setVida (int v) {
+                vida = v;
+        }
+        public void setFila (int f) {
+                fila = f;
+        }
+        public void setColumna (String c) {
+                columna = c;
+        }
 
-            public int getVida ()
-            {	return vida;	}
+        public String getNombre ()
+        {	return nombre;	}
+
+        public int getVida ()
+        {	return vida;	}
+
+        public int getFila ()
+        {	return fila;	}
+
+        public String getColumna ()
+        {	return columna;	}
             
-            public int getFila ()
-            {	return fila;	}
-            
-            public String getColumna ()
-            {	return columna;	}
+        public String toString ()
+        {
+            return    "NOMBRE:\t\t" +nombre +"\n"
+                    + "FILA:\t\t"   +fila   +"\n"
+                    + "COLUMNA:\t"  +columna+"\n"
+                    + "VIDA:\t\t"   +vida;
+        }
     }
 
--- VideoJuego2.java
+++ VideoJuego2.java
@@ -1,39 +1,76 @@
-public class VideoJuego2
-{
-    public static void main (String [] args)
+   
+    public class VideoJuego2
     {
-        Soldado [][] tablero = new Soldado [10][10];
-        inicializar (tablero);
-    }
-    
-    public static void inicializar (Soldado [][] tablero)
-    {
-        String columnaLetras [] = {"A","B","C","D","E","F","G","H","I","J"}; 
-        int     aleatorio = posicionRandom()+1, fila, columna;
-        
-        for (int i = 0 ; i < aleatorio ; i++)
+        public static void main (String [] args)
         {
-            // VERIFICA POSICION LIBRE
-            do
+            Soldado [][] tablero = new Soldado [10][10];
+            crear           (tablero);
+            mostrarTabla    (tablero);
+        }
+
+        public static void crear (Soldado [][] tablero)
+        {
+            int     aleatorio = posicionRandom()+1, fila, columna;
+
+            for (int i = 0 ; i < aleatorio ; i++)
             {
-                fila = posicionRandom();
-                columna = posicionRandom();
-            }
-            while ( tablero[fila][columna] != null );
+            // VERIFICA POSICION LIBRE
+                do
+                {
+                    fila = posicionRandom();
+                    columna = posicionRandom();
+                }
+                while ( tablero[fila][columna] != null );
             
+            // CREA E INICIALIZA DATOS DEL SOLDADO
             tablero[fila][columna] = new Soldado();
+            inicializar (tablero, fila, columna, i);
+            }
+        }
+        
+        public static void inicializar (Soldado [][] tablero, int f, int c, int cont)
+        {
+            String columnaLetras [] = {"A","B","C","D","E","F","G","H","I","J"};
+             
+            tablero[f][c].setNombre ("Soldado"+cont);
+            tablero[f][c].setVida   ( vida() );
+            tablero[f][c].setFila   (f+1);
+            tablero[f][c].setColumna(columnaLetras[c]);
+        }
+
+        public static void mostrarTabla (Soldado [][] tablero)
+        {
+            String letras [] = {"A","B","C","D","E","F","G","H","I","J"};
+            System.out.print ("     ");
             
+            for (int i = 0 ; i < letras.length ; i++)
+            {
+                System.out.print (letras[i]+"            ");
+            }
+            System.out.println(" ");
+            
+            for (int i = 0 ; i < tablero.length ; i++)
+            {
+                 System.out.print (i+1);
+                for (int j = 0 ; j < tablero[i].length ; j++)
+                {
+                    if (tablero[i][j] == null)
+                        System.out.print ("          |");
+                    else
+                        System.out.print (" "+tablero[i][j].getNombre()+" |");
+                }
+                System.out.println("\n--------------------------------------------------------------------------------------------------------------");
+            }
+        }
+        
+        public static int posicionRandom ()
+        {
+            return (int)(Math.random() * 10);
+        }
+
+        public static int vida ()
+        {
+            return (int)(Math.random() * 5 + 1);
         }
     }
-    
-    public static int posicionRandom ()
-    {
-        return (int)(Math.random() * 10);
-    }
-    
-    public static int vida ()
-    {
-        return (int)(Math.random() * 5 + 1);
-    }
-}
 
