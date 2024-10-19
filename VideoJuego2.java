7f94cdadd934f2e1a1a6108bef36e33dad87d952
leonhatches <jhatches@unsa.edu.pe>
2024-10-18 21:38:43 -0500

    4to commit con el laboratorio completo.

--- VideoJuego2.java
+++ VideoJuego2.java
@@ -3,28 +3,55 @@
     {
         public static void main (String [] args)
         {
+        	Soldado [] tableroUni;
             Soldado [][] tablero = new Soldado [10][10];
-            crear               (tablero);
+            
+            // Arreglos bidimensionales
+            tableroUni = crear  (tablero);
             mostrarTabla        (tablero);
             mostrarMayorVida    (tablero);
+            
             double vida = mostrarPromedioVida (tablero);
-            mostrar(tablero);
-
+            
             // MOSTRAR VIDA DE TODOS LOS SOLDADOS
-            System.out.println ("\n\t| VIDA TOTAL DEL EJERCITO |\n");
+            System.out.println ("\n| VIDA TOTAL DEL EJERCITO |");
             System.out.println ("La vida total es: " + vida );
+            
+
+            
+            // Arreglos estandar
+            System.out.println("\n| SOLDADOS - ORDEN DE CREACION |");
+            mostrar (tableroUni);
+            
+            System.out.println("\n| SOLDADOS - RANKING DE VIDA MAYOR A MENOR |");
+            mostrarRankingMayor (tableroUni);
+            
+            System.out.println("\n| SOLDADOS - RANKING DE VIDA MENOR A MAYOR |");
+            mostrarRankingMenor (tableroUni);
+           
+        }
+        
+        public static int posicionRandom ()
+        {
+            return (int) (Math.random() * 10);
         }
 
-        public static void crear (Soldado [][] tablero)
+        public static int vida ()
         {
-            int     aleatorio = posicionRandom()+1, fila, columna;
-
+            return (int) (Math.random() * 5 + 1);
+        }
+        
+        public static Soldado [] crear (Soldado [][] tablero)
+        {
+            int			aleatorio = posicionRandom()+1, fila, columna;
+            Soldado [] tableroUni = new Soldado[aleatorio];
+            
             for (int i = 0 ; i < aleatorio ; i++)
             {
             // VERIFICA POSICION LIBRE
                 do
                 {
-                    fila = posicionRandom();
+                    fila	= posicionRandom();
                     columna = posicionRandom();
                 }
                 while ( tablero[fila][columna] != null );
@@ -32,7 +59,12 @@
             // CREA E INICIALIZA DATOS DEL SOLDADO
             tablero[fila][columna] = new Soldado();
             inicializar (tablero, fila, columna, i);
+            
+            // ARREGLO ESTANDAR PARA TRABAJAR CON ORDENAMIENTO
+            tableroUni[i] = new Soldado ();
+            tableroUni[i] = tablero[fila][columna];
             }
+            return tableroUni;
         }
         
         public static void inicializar (Soldado [][] tablero, int f, int c, int cont)
@@ -44,41 +76,37 @@
             tablero[f][c].setFila   (f+1);
             tablero[f][c].setColumna(columnaLetras[c]);
         }
-
+        
         public static void mostrarTabla (Soldado [][] tablero)
         {
             String letras [] = {"A","B","C","D","E","F","G","H","I","J"};
             System.out.print ("     ");
             
+            // MUESTRA LAS COLUMNAS
             for (int i = 0 ; i < letras.length ; i++)
             {
-                System.out.print (letras[i]+"            ");
+                System.out.print (letras[i]+"          ");
             }
             System.out.println(" ");
             
             for (int i = 0 ; i < tablero.length ; i++)
             {
+            	// MUESTRA LAS FILAS
                 System.out.print (i+1);
                 for (int j = 0 ; j < tablero[i].length ; j++)
                 {
+                	// SI NO EXISTE SOLDADO, MUESTRA UN ESPACIO EN BLANCO
                     if (tablero[i][j] == null)
                         System.out.print ("          |");
                     else
                         System.out.print (" "+tablero[i][j].getNombre()+" |");
                 }
-                System.out.println("\n--------------------------------------------------------------------------------------------------------------");
+                System.out.println("\n-------------------------------------------------"
+                	  + "-------------------------------------------------------------");
             }
         }
         
-        public static int posicionRandom ()
-        {
-            return (int)(Math.random() * 10);
-        }
-
-        public static int vida ()
-        {
-            return (int)(Math.random() * 5 + 1);
-        }
+        // Uso de metodos con el arreglo bidimensional para la demostracion de conocimiento en este mismo.
         
         public static void mostrarMayorVida (Soldado[][]tablero)
         {
@@ -129,34 +157,56 @@
            }
            
             // MOSTRAR
-            System.out.println ("\n\t| PROMEDIO DE VIDA |\n");
+            System.out.println ("\n| PROMEDIO DE VIDA |");
             System.out.println ("El promedio de vida es: " + (suma/cont) );
             
             return suma;
         }
         
-        public static void mostrarPorCreacion (Soldado[][]tablero)
+        // Desde ahora, metodos con arreglo estandar para trabajar el ordenamiento o metodos que lo requieran.
+        
+        public static void mostrarRankingMayor (Soldado [] tablero)
+        { 
+        	Soldado intercambio;
+        	
+        	// Algoritmo de ordenamiento Insercion
+    		for (int i = 1 ; i < tablero.length ; i++)
+    		{
+    			for (int r = i ; r > 0 && tablero[r-1].getVida() < tablero[r].getVida() ; r--)
+    			{
+    				intercambio   = tablero [r-1];
+    				tablero [r-1] = tablero   [r];
+    				tablero   [r] = intercambio;
+    			}
+    		}
+    		
+    		mostrar (tablero);
+        }
+        
+        public static void mostrarRankingMenor (Soldado [] tablero)
         {
+            Soldado intercambio;
             
-        }
-        
-        public static void mostrar (Soldado [][] tablero)
-        {
-           System.out.println("\n\t| SOLDADOS - EJERCITO |");
-           for (int i = 0 ; i < tablero.length ; i++)
-           {
-                for (int j = 0 ; j < tablero[i].length ; j++){
-                    if ( !(tablero[i][j] == null) )
+            // Algoritmo de ordenamiento Burbuja
+            for (int i = tablero.length - 1 ; i > 0 ; i--)
+                for (int n = 0 ; n < i ; n++)
+                {
+                    if ( tablero[n].getVida() > tablero[n+1].getVida() )
                     {
-                         System.out.println("\n"+tablero[i][j]);
-                    }       
+                        intercambio  = tablero[n];
+                        tablero[n]   = tablero[n+1];
+                        tablero[n+1] = intercambio;
+                    }
                 }
-           }
+            
+            mostrar(tablero);
         }
         
-        
-        
-        
-        
+        public static void mostrar (Soldado [] tablero)
+	    {
+            for (int i = 0 ; i < tablero.length ; i++)
+                System.out.println("\t| SOLDADO N-"+(i+1)+" |\n"+tablero[i]+"\n");
+	    }
+  
     }
 
